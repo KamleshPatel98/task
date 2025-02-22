e915fcec76bef36b4569f166d6ac23594998c869
KamleshPatel98 <kamleshp52170@gmail.com>
2025-02-22 19:11:12 +0530

    validate

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -7,10 +7,21 @@ use Illuminate\Http\Request;
 use Hash;
 use App\Models\User;
 use Auth;
+use Validator;
 
 class AuthController extends Controller
 {
     public function register(Request $request){
+        $validator = Validator::make($request->all(),[
+            'name'=>'required|string|max:255',
+            'email'=>'required|unique:users|email|max:255',
+            'password'=>'required|min:8|max:12|string'
+        ]);
+        if($validator->fails()){
+            $errorMessage = $validator->errors()->first();
+            return response()->json(['status'=>true,'message'=>'Validation error!','error'=>$errorMessage],422);
+        }
+
         try {
             User::create([
                 'name' => $request->name,
@@ -24,13 +35,21 @@ class AuthController extends Controller
     }
 
     public function login(Request $request){
+        $validator = Validator::make($request->all(),[
+            'email'=>'required|exists:users|email|max:255',
+            'password'=>'required|min:8|max:12|string'
+        ]);
+        if($validator->fails()){
+            $errorMessage = $validator->errors()->first();
+            return response()->json(['status'=>true,'message'=>'Validation error!','error'=>$errorMessage],422);
+        }
+    
         try {
-            $user = User::where('email', $request->email)->first();
-            if (!$user || !Hash::check($request->password, $user->password)) {
+            if(!Auth::attempt(['email' => $request->email, 'password' => $request->password])){
                 return response()->json(['message' => 'Invalid credentials'], 401);
             }
+            $user = Auth::user();
             $token = $user->createToken('auth_token')->plainTextToken;
-
             $user['token'] = $token;
 
             return response()->json(['status'=>true,'message'=>'otp match successfully','data'=>$user],200);
